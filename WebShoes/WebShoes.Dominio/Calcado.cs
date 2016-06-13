665c67a0ea39ceac14eafa508e1ecfe365a3b943
Rafael <rafael@campotv.com.br>
2016-06-13 20:24:37 -0300

    Classe de Dominio Calcado Add campos

---
+++ WebShoes/WebShoes.Dominio/Calcado.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShoes.Dominio
+{
+    public class Calcado
+    {
+        public Calcado(string _nome, string _marca, string _cor, int _tamanho)
+        {
+            Modelo = _nome;
+            Marca = _marca;
+            Cor = _cor;
+            Tamanho = _tamanho;
+        }
+
+        public string Cor { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public object Tamanho { get; set; }
+    }
+}
---
+++ WebShoes/WebShoes.Testes.Dominio/CalcadoTeste.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebShoes.Dominio;
+
+namespace WebShoes.Testes.Dominio
+{
+    [TestClass]
+    public class CalcadoTeste
+    {
+        [TestMethod]
+        public void CriarCalcadoTeste()
+        {
+            Calcado calcado = new Calcado("TÊNIS NIKE AIR JORDAN 1 LOW", "Nike", "Vermelho", 41);
+            Assert.AreEqual("TÊNIS NIKE AIR JORDAN 1 LOW - Nike - Vermelho - 41", calcado.toString());
+        }
+    }
+}
