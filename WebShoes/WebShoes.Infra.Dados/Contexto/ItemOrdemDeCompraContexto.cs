c8a9a494b39710a48d99265a04e0bcf5c7bffe3b
brunobuck <brunob_schmidt@hotmail.com>
2016-06-13 21:48:41 -0300

    Inclusão ItemdeCompra Dados

---
+++ WebShoes/WebShoes.Infra.Dados/Contexto/ItemOrdemDeCompraContexto.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using WebShoes.Dominio;
+
+namespace WebShoes.Infra.Dados.Contexto
+{
+    public class ItemOrdemDeCompraContexto : DbContext
+    {
+        public ItemOrdemDeCompraContexto() : base("CalcadoDB")
+        {
+        }
+
+        public DbSet<ItemOrdemDeCompra> Itens { get; set; }
+    }
+}
---
+++ WebShoes/WebShoes.Testes.Dados/ItemOrdemDeCompraTesteDados.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebShoes.Infra.Dados.Contexto;
+using WebShoes.Dominio.Contratos;
+using System.Data.Entity;
+using WebShoes.Testes.Dados.Base;
+using WebShoes.Infra.Dados.Repositorio;
+
+namespace WebShoes.Testes.Dados
+{
+    /// <summary>
+    /// Summary description for ItemOrdemDeCompraTesteDados
+    /// </summary>
+    [TestClass]
+    public class ItemOrdemDeCompraTesteDados
+    {
+        private ItemOrdemDeCompraContexto _contexto;
+        private IItemOrdemDeCompraRepositorio _repositorio;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            Database.SetInitializer(new CriarNovoBancoCalcado<ItemOrdemDeCompraContexto>());
+
+            _contexto = new ItemOrdemDeCompraContexto();
+            _repositorio = new ItemOrdemDeCompraRepositorio();
+
+            _contexto.Database.Initialize(true);
+
+            _contexto.Database.Log = s => System.Diagnostics.Debug.WriteLine(s);
+        }
+
+        [TestMethod]
+        public void CriarItemOrdemDeCompraRepositorioTeste()
+        {
+        }
+
+        [TestMethod]
+        public void RetornarItemOrdemDeCompraRepositorioTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void RetornaTodosOsItemOrdemDeCompraRepositorioTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void AtualizaItemOrdemDeCompraRepositorioTeste()
+        {
+            
+
+        }
+
+        [TestMethod]
+        public void DeletarItemOrdemDeCompraRepositorioTeste()
+        {
+            
+        }
+    }
+}
